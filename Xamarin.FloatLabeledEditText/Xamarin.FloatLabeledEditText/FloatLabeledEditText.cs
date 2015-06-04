ed668d622ca22d80de9ab4536e44ed6eecfdb2d4
William Cossey <wcossey@bitbucket.org>
2015-06-04 20:58:08 +0200

    Initial Commit

---
+++ Xamarin.FloatLabeledEditText/Xamarin.FloatLabeledEditText/FloatLabeledEditText.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using Android.Animation;
+using Android.Annotation;
+using Android.Content;
+using Android.Content.Res;
+using Android.Graphics.Drawables;
+using Android.OS;
+using Android.Runtime;
+using Android.Text;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using Java.Lang;
+
+namespace Xamarin.FloatLabeledEditText
+{
+  public class FloatLabeledEditText : FrameLayout
+  {
+    private const int DEFAULT_PADDING_LEFT = 2;
+
+    private TextView _hintTextView;
+    private EditText _editText;
+
+    private readonly Context _context;
+
+
+    protected FloatLabeledEditText(IntPtr javaReference, JniHandleOwnership transfer)
+      : base(javaReference, transfer)
+    {
+    }
+
+    public FloatLabeledEditText(Context context)
+      : base(context)
+    {
+      _context = context;
+    }
+
+    public FloatLabeledEditText(Context context, IAttributeSet attrs)
+      : base(context, attrs)
+    {
+      _context = context;
+      SetAttributes(attrs);
+    }
+
+    [TargetApi(Value = (int) BuildVersionCodes.Honeycomb)]
+    public FloatLabeledEditText(Context context, IAttributeSet attrs, int defStyleAttr)
+      : base(context, attrs, defStyleAttr)
+    {
+      _context = context;
+      SetAttributes(attrs);
+    }
+
+    public FloatLabeledEditText(Context context, IAttributeSet attrs, int defStyleAttr, int defStyleRes)
+      : base(context, attrs, defStyleAttr, defStyleRes)
+    {
+    }
+
+    private void SetAttributes(IAttributeSet attrs)
+    {
+      _hintTextView = new TextView(_context);
+
+      var a = _context.ObtainStyledAttributes(attrs, Resource.Styleable.FloatLabeledEditText);
+
+      var padding = a.GetDimensionPixelSize(Resource.Styleable.FloatLabeledEditText_fletPadding, 0);
+      var defaultPadding =
+        (int) TypedValue.ApplyDimension(ComplexUnitType.Dip, DEFAULT_PADDING_LEFT, Resources.DisplayMetrics);
+      var paddingLeft = a.GetDimensionPixelSize(Resource.Styleable.FloatLabeledEditText_fletPaddingLeft, defaultPadding);
+      var paddingTop = a.GetDimensionPixelSize(Resource.Styleable.FloatLabeledEditText_fletPaddingTop, 0);
+      var paddingRight = a.GetDimensionPixelSize(Resource.Styleable.FloatLabeledEditText_fletPaddingRight, 0);
+      var paddingBottom = a.GetDimensionPixelSize(Resource.Styleable.FloatLabeledEditText_fletPaddingBottom, 0);
+      var background = a.GetDrawable(Resource.Styleable.FloatLabeledEditText_fletBackground);
+
+      if (padding != 0)
+      {
+        _hintTextView.SetPadding(padding, padding, padding, padding);
+      }
+      else
+      {
+        _hintTextView.SetPadding(paddingLeft, paddingTop, paddingRight, paddingBottom);
+      }
+
+      if (background != null)
+      {
+        setHintBackground(background);
+      }
+
+      _hintTextView.SetTextAppearance(_context,
+        a.GetResourceId(Resource.Styleable.FloatLabeledEditText_fletTextAppearance,
+          Android.Resource.Style.TextAppearanceSmall));
+
+      //Start hidden
+      _hintTextView.Visibility = ViewStates.Invisible;
+      //AnimatorProxy.wrap(mHintTextView).setAlpha(0);
+
+      AddView(_hintTextView, ViewGroup.LayoutParams.WrapContent, ViewGroup.LayoutParams.WrapContent);
+
+      a.Recycle();
+    }
+
+    [SuppressLint(Value = new[] {"NewApi"})]
+    private void setHintBackground(Drawable background)
+    {
+      if (Build.VERSION.SdkInt >= BuildVersionCodes.JellyBean)
+      {
+        _hintTextView.Background = background;
+      }
+      else
+      {
+        _hintTextView.SetBackgroundDrawable(background);
+      }
+    }
+
+
+    public override void AddView(View child, int index, ViewGroup.LayoutParams @params)
+    {
+      if (child.GetType() == typeof (EditText))
+      {
+        if (_editText != null)
+        {
+          throw new IllegalArgumentException("Can only have one EditText subview");
+        }
+
+        var lp = new LayoutParams(@params)
+        {
+          Gravity = GravityFlags.Bottom,
+          TopMargin = (int) (_hintTextView.TextSize + _hintTextView.PaddingBottom + _hintTextView.PaddingTop)
+        };
+        @params = lp;
+
+        setEditText((EditText) child);
+      }
+
+
+      base.AddView(child, index, @params);
+    }
+
+
+    private void setEditText(EditText editText)
+    {
+      _editText = editText;
+
+      _editText.AfterTextChanged += delegate(object sender, AfterTextChangedEventArgs args)
+      {
+        setShowHint(!TextUtils.IsEmpty(args.Editable));
+      };
+
+      _editText.BeforeTextChanged += delegate(object sender, TextChangedEventArgs args)
+      {
+        
+      };
+
+      _editText.TextChanged += delegate(object sender, TextChangedEventArgs args)
+      {
+        
+      };
+      
+      _editText.FocusChange += delegate(object sender, FocusChangeEventArgs args)
+      {
+        if (args.HasFocus && _hintTextView.Visibility == ViewStates.Visible)
+        {
+          ObjectAnimator.OfFloat(_hintTextView, "alpha", 0.33f, 1f).Start();
+        }
+        else if (_hintTextView.Visibility == ViewStates.Visible)
+        {
+          //AnimatorProxy.wrap(mHintTextView).setAlpha(1f); //Need this for compat reasons
+          ObjectAnimator.OfFloat(_hintTextView, "alpha", 1f, 0.33f).Start();
+        }
+      };
+     
+
+      _hintTextView.SetText(_editText.Hint, TextView.BufferType.Normal);
+
+      if (!string.IsNullOrEmpty(_editText.Text))
+      {
+        _hintTextView.Visibility = ViewStates.Visible;
+      }
+    }
+
+
+    private void setShowHint(bool show)
+    {
+      AnimatorSet animation = null;
+
+      if ((_hintTextView.Visibility == ViewStates.Visible) && !show)
+      {
+        animation = new AnimatorSet();
+        var move = ObjectAnimator.OfFloat(_hintTextView, "translationY", 0, _hintTextView.Height/8);
+        var fade = ObjectAnimator.OfFloat(_hintTextView, "alpha", 1, 0);
+        animation.PlayTogether(move, fade);
+      }
+      else if ((_hintTextView.Visibility != ViewStates.Visible) && show)
+      {
+        animation = new AnimatorSet();
+        var move = ObjectAnimator.OfFloat(_hintTextView, "translationY", _hintTextView.Height/8, 0);
+        var fade = ObjectAnimator.OfFloat(_hintTextView, "alpha", 0, _editText.IsFocused ? 1 : 0.33f);
+        animation.PlayTogether(move, fade);
+      }
+
+      if (animation == null) 
+        return;
+
+      animation.AnimationStart += delegate
+      {
+        base.OnAnimationStart(); 
+        _hintTextView.Visibility = ViewStates.Visible;
+      };
+
+      animation.AnimationEnd += delegate
+      {
+        base.OnAnimationEnd();
+        _hintTextView.Visibility = show ? ViewStates.Visible : ViewStates.Invisible;
+        //AnimatorProxy.wrap(_hintTextView).setAlpha(show ? 1 : 0);
+      };
+
+      animation.Start();
+    }
+
+    public EditText getEditText()
+    {
+      return _editText;
+    }
+
+    public void setHint(string hint)
+    {
+      _editText.Hint = hint;
+      _hintTextView.Text = hint;
+    }
+
+    public ICharSequence getHint()
+    {
+      return new Java.Lang.String(_hintTextView.Hint);
+    }
+  }
+}
---
+++ Xamarin.FloatLabeledEditText/Xamarin.FloatLabeledEditText/Utils.cs
@@ -0,0 +1,42 @@
+﻿using Android.Annotation;
+using Android.OS;
+using Android.Views;
+using Java.Util.Concurrent.Atomic;
+
+namespace Xamarin.FloatLabeledEditText
+{
+  public class Utils
+  {
+    private static readonly AtomicInteger NextGeneratedId = new AtomicInteger(1);
+
+    /// <summary>
+    /// <para>Generate a value suitable for use in {@link #setId(int)}.<br/>
+    /// This value will not collide with ID values generated at build time by aapt for R.id.</para>
+    /// </summary>
+    /// <returns>a generated ID value</returns>
+    private static int generateViewId()
+    {
+      for (;;)
+      {
+        var result = NextGeneratedId.Get();
+
+        // aapt-generated IDs have the high byte nonzero; clamp to the range under that.
+        var newValue = result + 1;
+
+        if (newValue > 0x00FFFFFF)
+          newValue = 1; // Roll over to 1, not 0.
+
+        if (NextGeneratedId.CompareAndSet(result, newValue))
+        {
+          return result;
+        }
+      }
+    }
+
+    [SuppressLint(Value = new[] { "NewApi" })]
+    public static int generateId()
+    {
+      return Build.VERSION.SdkInt < BuildVersionCodes.JellyBeanMr1 ? generateViewId() : View.GenerateViewId();
+    }
+  }
+}
